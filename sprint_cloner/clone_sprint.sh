0e77a0a925975d4361975578ace6567eded2fa7b
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-04-16 09:41:43 -0400

    remove one extra line

--- sprint_cloner/clone_sprint.sh
+++ sprint_cloner/clone_sprint.sh
@@ -14,7 +14,6 @@ declare -A Branches=()
 GH_URL="https://github.com/"
 SC_URL="/Sprint-Challenge--"
 PATH_TO_STUDENTS="<fill in your path here>"
-PATH_TO_STUDENTS="~/Desktop/repos/students/"
 # clone student sprint branch to their student folder
 git clone -b "${Branches[$name]}" "$GH_URL""${Students[$name]}""$SC_URL""$sprint"".git" "$PATH_TO_STUDENTS""$name"/"$sprint"-SC
 
